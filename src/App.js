07572bd8bf5d434cb7950d92646315420c62f895
naotaro0123 <n.ikewada@gmail.com>
2019-05-04 02:09:48 +0900

    deck.glでmapboxを表示

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import MapGL from 'react-map-gl';
+import { MapStylePicker } from './modules/controls';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
+
+export default class App extends Component {
+  state = {
+    style: 'mapbox://styles/mapbox/light-v9',
+    viewport: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      longitude: -74,
+      latitude: 40.7,
+      zoom: 11,
+      maxZoom: 16
+    }
+  };
+
+  componentDidMount() {
+    window.addEventListener('resize', this._resize);
+    this._resize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this._resize);
+  }
+
+  onStyleChange = style => {
+    this.setState({ style });
+  };
+
+  _onViewportChange = viewport => {
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
+    });
+  };
+
+  _resize = () => {
+    this._onViewportChange({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <MapStylePicker
+          onStyleChange={this.onStyleChange}
+          currentStyle={this.state.style}
+        />
+        <MapGL
+          {...this.state.viewport}
+          mapStyle={this.state.style}
+          onViewportChange={viewport => this._onViewportChange(viewport)}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+        />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/modules/deckgl-layers.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+
+const PICKUP_COLOR = [114, 19, 108];
+const DROPOFF_COLOR = [243, 185, 72];
+
+export function renderLayers(props) {
+  return [];
+}
