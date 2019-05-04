53416ef09b9463d395ba7b00464a56456ae3f0ee
naotaro0123 <n.ikewada@gmail.com>
2019-05-04 22:43:33 +0900

    Scatterplot完了、チュートリアルが間違ってる

--- src/Scatterplot/Scatterplot.js
+++ src/Scatterplot/Scatterplot.js
@@ -4,13 +4,19 @@ import { StaticMap } from 'react-map-gl';
 import DeckGL from 'deck.gl';
 import taxiData from '../data/taxi';
 import { renderLayers } from '../modules/deckgl-layers';
+import {
+  LayerControls,
+  MapStylePicker,
+  SCATTERPLOT_CONTROLS
+} from '../modules/controls';
+import { tooltipStyle } from '../modules/style';
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 
 const INITIAL_VIEW_STATE = {
   longitude: -74,
   latitude: 40.74,
-  zoom: 16,
+  zoom: 11,
   minZoom: 5,
   maxZoom: 20,
   pitch: 0,
@@ -19,12 +25,33 @@ const INITIAL_VIEW_STATE = {
 
 export default class Scatterplot extends React.Component {
   state = {
+    hover: {
+      x: 0,
+      y: 0,
+      hoveredObject: null
+    },
     points: [],
+    settings: Object.keys(SCATTERPLOT_CONTROLS).reduce(
+      (accu, key) => ({
+        ...accu,
+        [key]: SCATTERPLOT_CONTROLS[key].value
+      }),
+      {}
+    ),
     style: 'mapbox://styles/mapbox/light-v9'
   };
 
   componentDidMount() {
     this._processData();
+  }
+
+  _updateLayerSettings(settings) {
+    this.setState({ settings });
+  }
+
+  _onHover({ x, y, object }) {
+    const label = object ? (object.pickup ? 'Pickup' : 'Dropoff') : null;
+    this.setState({ hover: { x, y, hoveredObject: object, label } });
   }
 
   _processData() {
@@ -52,10 +79,38 @@ export default class Scatterplot extends React.Component {
   };
 
   render() {
+    const data = this.state.points;
+    if (!data.length) {
+      return null;
+    }
+    const { hover, settings } = this.state;
     return (
       <div>
+        {hover.hoveredObject && (
+          <div
+            style={{
+              ...tooltipStyle,
+              transform: `translate(${hover.x}px, ${hover.y}px)`
+            }}
+          >
+            <div>{hover.label}</div>
+          </div>
+        )}
+        <MapStylePicker
+          onStyleChange={this.onStyleChange}
+          currentStyle={this.state.style}
+        />
+        <LayerControls
+          settings={this.state.settings}
+          propTypes={SCATTERPLOT_CONTROLS}
+          onChange={settings => this._updateLayerSettings(settings)}
+        />
         <DeckGL
-          layers={renderLayers({ data: this.state.points })}
+          layers={renderLayers({
+            data: this.state.points,
+            onHover: hover => this._onHover(hover),
+            settings: this.state.settings
+          })}
           initialViewState={INITIAL_VIEW_STATE}
           controller
         >
--- src/modules/deckgl-layers.js
+++ src/modules/deckgl-layers.js
@@ -1,23 +1,24 @@
 /* eslint-disable no-unused-vars */
-import React, { Component } from 'react';
 import { ScatterplotLayer } from 'deck.gl';
 
 const PICKUP_COLOR = [114, 19, 108];
 const DROPOFF_COLOR = [243, 185, 72];
 
 export function renderLayers(props) {
-  const { data } = props;
+  const { data, onHover, settings } = props;
   return [
     new ScatterplotLayer({
       id: 'scatterplot',
       getPosition: d => d.position,
-      getColor: d => [0, 128, 255],
+      getColor: d => (d.pickup ? PICKUP_COLOR : DROPOFF_COLOR),
       getRadius: d => 5,
       opacity: 0.5,
       pickable: true,
       radiusMinPixels: 0.25,
-      radiusMaxPixels: 30,
-      data
+      radiusMaxPixels: 20,
+      data,
+      onHover,
+      ...settings
     })
   ];
 }
